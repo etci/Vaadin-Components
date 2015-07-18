9b5b870c5ebe09aa3b3e34c445754b157da1e4f0
User <User@Asus>
2015-07-18 18:24:49 +0300

    Initial

---
+++ CustomTable/target/.generated/com/google/gwt/lang/tr_00046biz_00046vardar_00046customtable_00046MyAppWidgetset__EntryMethodHolder.java
@@ -0,0 +1,6 @@
+package com.google.gwt.lang;
+public class tr_00046biz_00046vardar_00046customtable_00046MyAppWidgetset__EntryMethodHolder {
+  public static final void init() {
+    // to be synthesized later
+  }
+}
